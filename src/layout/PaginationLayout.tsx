461b83eef9b2b101d7c483044a514006b45b23c4
Vadim-Moroz <morozvadim1804@gmail.com>
2026-09-18 18:33:13 +0300

    hw4 task1

--- src/layout/PaginationLayout.tsx
+++ src/layout/PaginationLayout.tsx
@@ -1,12 +1,17 @@
-import {Outlet} from "react-router-dom";
+import {Outlet, useNavigate} from "react-router-dom";
 import PaginationComponent from "../component/pagination-component/PaginationComponent.tsx";
+import {useEffect} from "react";
 
 
 const PaginationLayout = () => {
+    const navigate = useNavigate();
+    useEffect(() => {
+        navigate("products");
+    }, [navigate]);
     return (
         <>
-            <Outlet/>
             <PaginationComponent/>
+            <Outlet/>
         </>
     );
 };
--- src/services/api.service.tsx
+++ src/services/api.service.tsx
@@ -3,5 +3,5 @@ import type {ResponseIProducts} from "../model/ResponseIProducts.ts";
 export const getProducts = async (page:string):Promise<ResponseIProducts> => {
     const limit=10
     const skip=limit * (+page)-limit
-    return await fetch('https://dummyjson.com/products' + '?skip=' + skip).then(res => res.json())
+    return await fetch('https://dummyjson.com/products'+'?limit='+ limit + ';skip=' + skip).then(res => res.json())
 }
